e5d77282fe7c3a3e69fe538c241c9eb6b8250ef9
coder.yoon <yjin830201@gmail.com>
2023-02-15 00:57:50 +0900

    validation and sanitization

--- router/tweets.js
+++ router/tweets.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { body } from "express-validator";
 import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+
 const router = express.Router();
 
+// validation
+const validateTweet = [
+  body("text").trim().isLength({ min: 3 }).withMessage("3자 이상 입력해"),
+  validate,
+];
 // GET /tweets
 // GET /tweets?username=:username
 router.get("/", tweetController.getTweets);
@@ -10,10 +18,10 @@ router.get("/", tweetController.getTweets);
 router.get("/:id", tweetController.getTweet);
 
 // POST /tweets
-router.post("/", tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete("/:id", tweetController.deleteTweet);
