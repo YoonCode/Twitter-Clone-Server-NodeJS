43035cfc298078b5ef45b4f8f560e515c862d862
coder.yoon <yjin830201@gmail.com>
2023-02-17 01:13:16 +0900

    auth added

--- data/tweet.js
+++ data/tweet.js
@@ -2,23 +2,26 @@ let tweets = [
   {
     id: "1",
     text: "풀스택 개발자가 되자!",
-    createdAt: Date.now().toString(),
-    name: "Sangjin",
-    username: "sangjin",
-    url: "https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg",
+    createdAt: new Date().toString(),
+    userId: "1",
   },
   {
     id: "2",
     text: "올해도 화이팅!",
-    createdAt: Date.now().toString(),
-    name: "Jinsol",
-    username: "jinsol",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+    createdAt: new Date().toString(),
+    userId: "1",
   },
 ];
 
 export async function getAll() {
-  return tweets;
+  return Promise.all(
+    tweets.map(async tweet => {
+      const { username, name, url } = await userRepository.findById(
+        tweet.userId
+      );
+      return { ...tweet, username, name, url };
+    })
+  );
 }
 
 export async function getAllByUsername(username) {
@@ -26,19 +29,23 @@ export async function getAllByUsername(username) {
 }
 
 export async function getById(id) {
-  return tweets.find(t => t.id === id);
+  const found = tweets.find(tweet => tweet.id === id);
+  if (!found) {
+    return null;
+  }
+  const { username, name, url } = await userRepository.findById(found.userId);
+  return { ...found, username, name, url };
 }
 
-export async function create(text, name, username) {
+export async function create(text, userId) {
   const tweet = {
     id: Date.now().toString(),
     text,
     createdAt: new Date(),
-    name,
-    username,
+    userId,
   };
   tweets = [tweet, ...tweets];
-  return tweet;
+  return getById(tweet.id);
 }
 
 export async function update(id, text) {
@@ -46,7 +53,7 @@ export async function update(id, text) {
   if (tweet) {
     tweet.text = text;
   }
-  return tweet;
+  return getById(tweet.id);
 }
 
 export async function remove(id) {
