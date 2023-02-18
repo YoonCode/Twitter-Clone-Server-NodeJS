7e5ba735ed3415d8df5981331c597876a1992fac
coder.yoon <yjin830201@gmail.com>
2023-02-18 22:34:48 +0900

    mvc: data and controller added

---
+++ data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+  {
+    id: "1",
+    text: "NodeJS 공부 화이팅",
+    createdAt: new Date().toString(),
+    name: "Sangjin",
+    username: "sangjin",
+    url: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/3d/3d0c7a38612d1fba0e91e473af6c594799b31429.jpg",
+  },
+  {
+    id: "2",
+    text: "회계 공부 화이팅",
+    createdAt: new Date().toString(),
+    name: "Jinsol",
+    username: "jinsol",
+    url: "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/3d/3d0c7a38612d1fba0e91e473af6c594799b31429.jpg",
+  },
+];
+
+export function getAll() {
+  return tweets;
+}
+
+export function getAllByUsername(username) {
+  return tweets.filter(t => t.username === username);
+}
+
+export function getById(id) {
+  return tweets.find(t => t.id === id);
+}
+
+export function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date().toString(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweets;
+}
+
+export function update(id, text) {
+  const tweet = tweets.find(t => t.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export function remove(id) {
+  tweets = tweets.filter(t => t.id !== id);
+}
