d231d4cabb2506fbc44af124e3fe0f995ef7d07f
coder.yoon <yjin830201@gmail.com>
2023-02-20 01:41:07 +0900

    validation & sanitization

--- router/tweets.js
+++ router/tweets.js
@@ -1,7 +1,21 @@
 import express from "express";
+import { body } from "express-validator";
 import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
+
+// validation: 데이터를 서버에 저장하기 전에 검증하기 위함
+// sanitization: 데이터를 일관성 있게 보관하기 위함
+// Contract Testing: Client-Server 간의 데이터 교환 규약을 테스트하는 것
+
+const validateTweet = [
+  body("text") //
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 chars"),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -9,9 +23,9 @@ router.get("/", tweetController.getTweets);
 // GET /tweets/:id
 router.get("/:id", tweetController.getTweet);
 // POST /tweets
-router.post("/", tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 // PUT /tweets/:id
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 // DELETE /tweets/:id
 router.delete("/:id", tweetController.deleteTweet);
 
